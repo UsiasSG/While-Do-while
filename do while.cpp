39e1f7be60ad6edc3cb1ae15b82716e51a31c693
UsiasSG <160407522+UsiasSG@users.noreply.github.com>
2024-03-01 02:35:19 -0600

    Add files via upload

---
+++ do while.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+main(){
+	/*
+	int i = 5;  
+	while(i>0){
+		cout<<i<<endl;
+		i--;
+			
+	}
+	*/
+	char respuesta='s';
+
+	while(respuesta=='s' || respuesta=='s' ){
+
+		cout<<"Desea ingresar otro valor (s/n) ";
+		cin>>respuesta;
+		
+	    
+	}
+		
+	
+	system("pause");
+}
